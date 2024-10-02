dc948a1de2725a5b63d8c3ce511644f76eded271
NajibXY <naj.elkhadir@gmal.com>
2024-10-02 10:51:21 +0200

    fin td02

--- td02/Buddha.cs
+++ td02/Buddha.cs
@@ -22,26 +22,70 @@ public class Buddha : MonoBehaviour
 
         mesh.vertices = buddha.Item1;
         mesh.triangles = buddha.Item2.ToArray();
-        // todo reallocate normals
-        // mesh.normals = buddha.Item3.ToArray();
+        mesh.normals = buddha.Item3.ToArray();
+
+        WriteOnPath("Assets/Mesh/", "buddha.obj", mesh.vertices, mesh.triangles, mesh.normals);
     }
 
+    void WriteOnPath(string path, string name, Vector3[] vertices, int[] triangles, Vector3[] normals)
+    {
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, name)))
+        {
+            outputFile.WriteLine("# " + name.Split(' ')[0]);
+
+            outputFile.WriteLine("########################### VERTICES ###########################");
+
+            foreach (Vector3 vertice in vertices)
+                outputFile.WriteLine("v {0} {1} {2}",
+                    vertice.x.ToString(CultureInfo.InvariantCulture), 
+                    vertice.y.ToString(CultureInfo.InvariantCulture),
+                    vertice.z.ToString(CultureInfo.InvariantCulture)
+                );
+            
+            outputFile.WriteLine("########################### NORMALS ###########################");
+
+            foreach (Vector3 normal in normals)
+                outputFile.WriteLine("vn {0} {1} {2}",
+                    normal.x.ToString(CultureInfo.InvariantCulture), 
+                    normal.y.ToString(CultureInfo.InvariantCulture),
+                    normal.z.ToString(CultureInfo.InvariantCulture)
+                );
+
+            outputFile.WriteLine("########################### FACETS ###########################");
+
+            int i = 0;
+            while (i < triangles.Length) 
+            {
+                int index1 = triangles[i] + 1;
+                int index2 = triangles[i + 1] + 1;
+                int index3 = triangles[i + 2] + 1;
+
+                outputFile.WriteLine("f "
+                    + index1 + "//" + index1
+                    + " " + index2 + "//" + index2
+                    + " " + index3 + "//" + index3
+                );
+                i += 3;
+            }
+            Debug.Log("File written !");
+        }
+    }
     // Load Budda.off
     (Vector3[], List<int>, List<Vector3>) LoadPath(string path)
     {
         IEnumerable<string> lines = File.ReadLines(path);
         IEnumerator<string> enumerLines = lines.GetEnumerator();
         enumerLines.MoveNext();
-        Debug.Log(enumerLines.Current);
+        //Debug.Log(enumerLines.Current);
 
         enumerLines.MoveNext();
-        Debug.Log(enumerLines.Current);
+        //Debug.Log(enumerLines.Current);
 
         // Init vertices and listTriangles
         string[] values = enumerLines.Current.Split(' ');
         Vector3[] vertices = new Vector3[int.Parse(values[0])];
         List<int> listTriangles = new List<int>();
-        List<Vector3> normals = new List<Vector3>();
+        Vector3[] normals = new Vector3[int.Parse(values[0])];
         int i = 0;
         List<float> xs = new List<float>();
         List<float> ys = new List<float>();
@@ -54,10 +98,9 @@ public class Buddha : MonoBehaviour
             values = enumerLines.Current.Split(' ');
             if (values.Length == 3)
             {
-                Debug.Log("Vertice !");
+                //Debug.Log("Vertice !");
                 // Vertices
                 values = enumerLines.Current.Split(' ');
-                //todo enlever * 10
                 float x = float.Parse(values[0], CultureInfo.InvariantCulture) * 10.0f;
                 float y = float.Parse(values[1], CultureInfo.InvariantCulture) * 10.0f;
                 float z = float.Parse(values[2], CultureInfo.InvariantCulture) * 10.0f;
@@ -76,7 +119,7 @@ public class Buddha : MonoBehaviour
             } 
             else if (values.Length == 5)
             {
-                Debug.Log("Triangle !");
+                //Debug.Log("Triangle !");
 
                 // Index of Triangles
                 i1 = int.Parse(values[1]);
@@ -86,11 +129,13 @@ public class Buddha : MonoBehaviour
                 listTriangles.Add(i2);
                 listTriangles.Add(i3);
 
-                // Normal of the triangle
+                // Normal of the triangle applied to each vertex
                 p1 = vertices[i1];
                 p2 = vertices[i2];
                 p3 = vertices[i3];
-                normals.Add(Vector3.Scale(p2 - p1, p3-p1));
+                normals[i1]+= Vector3.Cross(p2 - p1, p3 - p1);
+                normals[i2]+= Vector3.Cross(p2 - p1, p3 - p1);
+                normals[i3]+= Vector3.Cross(p2 - p1, p3 - p1);
             }
         }
 
@@ -111,9 +156,8 @@ public class Buddha : MonoBehaviour
         foreach (Vector3 normal in normals)
         {
             normalizedNormals.Add(normal.normalized);
-            normalizedNormals.Add(normal.normalized);
-            normalizedNormals.Add(normal.normalized);
         }
+        
         return (vertices, listTriangles, normalizedNormals);
     }
 
