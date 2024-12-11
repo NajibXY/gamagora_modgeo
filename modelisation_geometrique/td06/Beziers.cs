5a949a701c26a1dc99ae29b88e9d5a6b01f2d25f
NajibXY <naj.elkhadir@gmail.com>
2024-12-11 09:52:47 +0100

    hermite and bernstein

--- modelisation_geometrique/td06/Beziers.cs
+++ modelisation_geometrique/td06/Beziers.cs
@@ -35,8 +35,6 @@ public class Beziers : MonoBehaviour
         if (points.Count > 0) 
             Gizmos.DrawLine(points[points.Count - 1], points[0]);
         
-        Color startColor = Color.green;
-        Color endColor = Color.blue;
         p0 = points[0];
         v1 = points[1];
         v0 = points[2];
@@ -45,21 +43,11 @@ public class Beziers : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
-    List<Vector3> newPoints = new List<Vector3>();
-    newPoints.Add(p0);
-    float t = delta;
-    while(t < 1.0f)
-    {
-        // float t = i / (float) numPoints - 1.0f;
-        Vector3 p = (2.0f * t * t * t - 3.0f * t * t + 1.0f) * p0 
-                    + (t * t * t - 2.0f * t * t + t) * v0 
-                    + (-2.0f * t * t * t + 3.0f * t * t) * p1 
-                    + (t * t * t - t * t) * v1;
-        newPoints.Add(p);
-        Debug.Log(p);
-        t += delta;
-    }
-    newPoints.Add(p1);
+    // Hermite
+    // List<Vector3> newPoints = Hermite();
+    
+    // Bernstein
+    List<Vector3> newPoints = Bernstein();
     
     // drawing
     Gizmos.color = Color.green;;
@@ -73,66 +61,66 @@ public class Beziers : MonoBehaviour
         
     Gizmos.color = Color.blue;
     Gizmos.DrawSphere(p1, 0.1f);
+    }
+
+    public List<Vector3> Bernstein()
+    {
+        List<Vector3> controlPoints = new List<Vector3>();
+        controlPoints.AddRange(points);
+        int n = controlPoints.Count - 1;
+        
+        List<Vector3> newPoints = new List<Vector3>();
+
+        float t = delta;
+        
+        // Bernstein polynomial
+        while (t < 1.0f)
+        {
+            Vector3 p = Vector3.zero;
+            for (int i = 0; i < controlPoints.Count; i++)
+            {
+                p += BinomialCoefficient(n, i) * controlPoints[i] * Mathf.Pow(1 - t, controlPoints.Count - 1 - i) * Mathf.Pow(t, i);
+            }
+            
+            newPoints.Add(p);
+            t += delta;
+        }
+
+        return newPoints;
+    }
     
-    //     if (points.Count > 0)
-    //     {
-    //         for (int i = 0; i < numPoints; i++)
-    //         {
-    //             float t = i / (float) numPoints;
-    //             Vector3 p = (2.0f * t * t * t - 3.0f * t * t + 1.0f) * p0 +
-    //                         (t * t * t - 2.0f * t * t + t) * v0 +
-    //                         (-2.0f * t * t * t + 3.0f * t * t) * p1 +
-    //                         (t * t * t - t * t) * v1;
-    //             newPoints.Add(p);
-    //         }
-    //         newPoints.Add(p1);
-    //
-    //         
-    //         
-    //         // drawing
-    //         Gizmos.color = startColor;
-    //         Gizmos.DrawSphere(p0, 0.1f);
-    //         
-    //         Gizmos.color = Color.Lerp(startColor, endColor, t);
-    //         for (int i = 0; i < newPoints.Count - 1; i++)
-    //         {
-    //             Gizmos.DrawLine(newPoints[i], newPoints[i + 1]);
-    //         }
-    //         
-    //         Gizmos.color = endColor;
-    //         Gizmos.DrawSphere(p1, 0.1f);
-    //
-    //         
-    //         // Chaikin Algorithm using subdivisions
-    //         // for (int i = 0; i < subdivisions; i++)
-    //         // {
-    //         //     List<Vector3> temp = new List<Vector3>();
-    //         //     for (int j = 0; j < newPoints.Count - 1; j++)
-    //         //     {
-    //         //         Vector3 p0 = newPoints[j];
-    //         //         Vector3 p1 = newPoints[j + 1];
-    //         //         Vector3 q0 = p0 + (p1 - p0) * 0.25f;
-    //         //         Vector3 q1 = p0 + (p1 - p0) * 0.75f;
-    //         //         temp.Add(q0);
-    //         //         temp.Add(q1);
-    //         //     }
-    //         //     Vector3 p00 = newPoints[newPoints.Count - 1];
-    //         //     Vector3 p11 = newPoints[0];
-    //         //     Vector3 q00 = p00 + (p11 - p00) * 0.25f;
-    //         //     Vector3 q11 = p00 + (p11 - p00) * 0.75f;
-    //         //     temp.Add(q00);
-    //         //     temp.Add(q11);
-    //         //     newPoints = temp;
-    //         // }
-    //         // Gizmos.color = Color.yellow;
-    //         // for (int i = 0; i < newPoints.Count - 1; i++)
-    //         // {
-    //         //     Gizmos.DrawLine(newPoints[i], newPoints[i + 1]);
-    //         // }
-    //         // Gizmos.DrawLine(newPoints[newPoints.Count - 1], newPoints[0]);
-    //         // Debug.Log(newPoints.Count);
-    //     }
-    //
-    //
+    private static int BinomialCoefficient(int n, int k)
+    {
+        if (k > n) return 0;
+        if (k == 0 || k == n) return 1;
+
+        int result = 1;
+        for (int i = 1; i <= k; i++)
+        {
+            result *= n - (k - i);
+            result /= i;
+        }
+        return result;
+    }
+
+    public List<Vector3> Hermite()
+    {
+        List<Vector3> newPoints = new List<Vector3>();
+        newPoints.Add(p0);
+        float t = delta;
+        while(t < 1.0f)
+        {
+            // float t = i / (float) numPoints - 1.0f;
+            Vector3 p = (2.0f * t * t * t - 3.0f * t * t + 1.0f) * p0 
+                        + (t * t * t - 2.0f * t * t + t) * v0 
+                        + (-2.0f * t * t * t + 3.0f * t * t) * p1 
+                        + (t * t * t - t * t) * v1;
+            newPoints.Add(p);
+            t += delta;
+        }
+        
+        newPoints.Add(p1);
+
+        return newPoints;
     }
 }
